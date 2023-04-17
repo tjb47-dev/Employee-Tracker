a54f1410933352d471b4700b20e553bf9fd49c3f
Thomas <tjb47.dev@gmail.com>
2023-04-17 17:08:48 -0400

    created the following functions, promptEmployeeRole prompt the user for an employee ID and role ID, addRole to add role to employee, promptAddRole to prompt to add the role.

--- server.js
+++ server.js
@@ -322,3 +322,131 @@ function employeeArray() {
     roleArray(employeeChoices);
   });
 }
+
+// Role Array
+// User choose the employee list, then employee is deleted
+function roleArray(employeeChoices) {
+  console.log("Updating an role");
+
+  var query = `SELECT r.id, r.title, r.salary 
+  FROM role r`;
+  let roleChoices;
+
+  connection.query(query, function (err, res) {
+    if (err) throw err;
+
+    roleChoices = res.map(({ id, title, salary }) => ({
+      value: id,
+      title: `${title}`,
+      salary: `${salary}`,
+    }));
+
+    console.table(res);
+    console.log("roleArray to Update!\n");
+
+    promptEmployeeRole(employeeChoices, roleChoices);
+  });
+}
+
+function promptEmployeeRole(employeeChoices, roleChoices) {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "employeeId",
+        message: "Which employee do you want to set with the role?",
+        choices: employeeChoices,
+      },
+      {
+        type: "list",
+        name: "roleId",
+        message: "Which role do you want to update?",
+        choices: roleChoices,
+      },
+    ])
+    .then(function (answer) {
+      var query = `UPDATE employee SET role_id = ? WHERE id = ?`;
+      // when finished prompting, insert a new item into the db with that info
+      connection.query(
+        query,
+        [answer.roleId, answer.employeeId],
+        function (err, res) {
+          if (err) throw err;
+
+          console.table(res);
+          console.log(res.affectedRows + "Updated successfully!");
+
+          firstPrompt();
+        }
+      );
+    });
+}
+
+// Add Role
+function addRole() {
+  var query = `SELECT d.id, d.name, r.salary AS budget
+    FROM employee e
+    JOIN role r
+    ON e.role_id = r.id
+    JOIN department d
+    ON d.id = r.department_id
+    GROUP BY d.id, d.name`;
+
+  connection.query(query, function (err, res) {
+    if (err) throw err;
+
+    // (callbackfn: (value: T, index: number, array: readonly T[]) => U, thisArg?: any)
+    const departmentChoices = res.map(({ id, name }) => ({
+      value: id,
+      name: `${id} ${name}`,
+    }));
+
+    console.table(res);
+    console.log("Department array!");
+
+    promptAddRole(departmentChoices);
+  });
+}
+
+// Prompt the user to add a role
+function promptAddRole(departmentChoices) {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "roleTitle",
+        message: "Role title?",
+      },
+      {
+        type: "input",
+        name: "roleSalary",
+        message: "Role Salary",
+      },
+      {
+        type: "list",
+        name: "departmentId",
+        message: "Department?",
+        choices: departmentChoices,
+      },
+    ])
+    .then(function (answer) {
+      var query = `INSERT INTO role SET ?`;
+
+      connection.query(
+        query,
+        {
+          title: answer.title,
+          salary: answer.salary,
+          department_id: answer.departmentId,
+        },
+        function (err, res) {
+          if (err) throw err;
+
+          console.table(res);
+          console.log("Role Inserted!");
+
+          firstPrompt();
+        }
+      );
+    });
+}
