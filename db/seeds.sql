6946a24c0d004f7a88029837c6191a8eb5609d4e
Thomas <tjb47.dev@gmail.com>
2023-04-17 17:48:38 -0400

    fixed error while inserting data into database. department was missing id and id's to those departments

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,18 +1,18 @@
 -- SQL query inserts data for multiple departments into the "department" table, including the department names. 
-INSERT INTO department (name) VALUES
-    ('parts'),
-    ('service'),
-    ('management');
+INSERT INTO department (name, id) VALUES
+    ('parts', 1),
+    ('service', 2),
+    ('management', 3);
 -- SQL query inserts data for multiple roles into the "role" table, including the job title and corresponding salary.
 INSERT INTO role (title, salary, department_id) VALUES 
-    ("porter", 20000, 1),
-    ("parts_driver", 30000, 2),
-    ("parts_specialist", 40000, 3),
-    ("mechanic", 50000, 4),
-    ("senior_mechanic", 70000, 5),
-    ("service_advisor", 80000, 6),
-    ("general_manager", 100000, 7),
-    ("director", 120000, 8);
+    ("porter", 20000, 2),
+    ("parts_driver", 30000, 1),
+    ("parts_specialist", 40000, 1),
+    ("mechanic", 50000, 2),
+    ("senior_mechanic", 70000, 2),
+    ("service_advisor", 80000, 2),
+    ("general_manager", 100000, 3),
+    ("director", 120000, 3);
 -- SQL query inserts data for multiple employees into the "employee" table, including their first name, last name, and manager's ID.
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES 
     ( "ava","johnson", 1, 7),
